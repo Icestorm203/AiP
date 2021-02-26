0729ce0c2a932e63ab61cf89f636802cc8b985d7
Icestorm <mustafa_2003_2003@mail.com>
2021-02-26 23:18:03 +0300

    first

---
+++ Git_1/Source.cpp
@@ -0,0 +1,18 @@
+#include "iostream";
+
+using namespace std;
+
+int main() {
+
+	cout << "**        **" << endl
+		<< "* *      * *" << endl
+		<< "*  *    *  *" << endl
+		<< "*   *  *   *" << endl
+		<< "*    *     *" << endl
+		<< "*          *" << endl
+		<< "*          *" << endl
+		<< "*          *" << endl;
+	getchar();
+	getchar();
+	return 0;
+}
